8b53b68e316451bd5f1fb5940ad76826881a34ff
David St-Hilaire <sthilaire.david@gmail.com>
2012-04-07 14:52:53 -0400

    added another test case and a benchmark. the benchmark can be run with 'go test dcpu -test.bench="Benchmark"'

--- dcpu/dcpu_test.go
+++ dcpu/dcpu_test.go
@@ -2,14 +2,43 @@ package dcpu
 
 import "testing"
 
-func TestSetRegister(t *testing.T) {
+func TestSetRegister0(t *testing.T) {
 	cpu := new(DCPU)
 	cpu.Init()
 	// SET A, 0x30 ; 7c01 0030
 	cpu.Loadprogram([]Word{0x7c01, 0x0030})
 	cpu.Step()
 	if cpu.reg[A] != 0x30 {
-		t.Errorf("Simple test failed: cpu.reg[A]: %x != 0x30", cpu.reg[A])
+		t.Errorf("Simple test 0 failed: cpu.reg[A]: %x != 0x30", cpu.reg[A])
+	}
+}
+
+func TestSetRegister1(t *testing.T) {
+	cpu := new(DCPU)
+	cpu.Init()
+	// SET A, 0x30 ; 7c01 0030
+	// SET PC, 0x0 ; 81c1
+	cpu.Loadprogram([]Word{0x7c01, 0x0030, 0x81c1})
+	cpu.Step()
+	cpu.Step()
+	if cpu.reg[A] != 0x30 {
+		t.Errorf("Simple test 1 failed: cpu.reg[A]: %x != 0x30", cpu.reg[A])
+	}
+
+	if cpu.pc != 0 {
+		t.Errorf("Simple test 1 failed: cpu.pc = %x != 0", cpu.pc)
+	}
+}
+
+func BenchmarkSetRegister(b *testing.B) {
+	cpu := new(DCPU)
+	cpu.Init()
+	// SET A, 0x30 ; 7c01 0030
+	// SET PC, 0x0 ; 81c1
+	cpu.Loadprogram([]Word{0x7c01, 0x0030, 0x81c1})
+
+	for i := 0 ; i < b.N ; i++ {
+		cpu.Step()
 	}
 }
 
